3cdb23a6b47efd9231b00a5698f2d1115c4be8ce
Sakarias Frette <sakarias.frette@outlook.com>
2022-03-11 15:28:58 +0100

    added readability

--- Project 1/main.c
+++ Project 1/main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <string.h>
+#include "read_graph_from_file.c"
 
 
 int main(int argc, char **argv){
-    char word[256];
-    
+    char *filename;
+    filename = argv[1];
+
     double d = atof(argv[2]);
     double epsilon = atof(argv[3]);
     int n = atoi(argv[4]);
-
+    read_graph_from_file(filename);
 }
--- Project 1/read_graph_from_file.c
+++ Project 1/read_graph_from_file.c
@@ -1,8 +1,37 @@
+#include <stdio.h>
+
+void read_graph_from_file(char *filename){//, int *N, int**row_ptr, int **col_idx, double **val){
+
+    FILE *fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char buff[255];
+    fp = fopen(filename, "r");
+
+    int nodes, edges;
+
+    char str1[10], str2[10];
+    
+
+    for (int i = 0; i < 1; ++i)
+        getline(&line, &len, fp); /* Skip the first 2 lines in file */
+
+    
+    getline(&line, &len, fp);
+    fscanf(fp, "%*s %*s %d %*s %d", &nodes, &edges );
+    printf("%d %d \n", nodes, edges);
+    
+    while ((read = getline(&line, &len, fp)) != -1) {
+        fscanf(fp, "%s %s", str1, str2);
+        printf("From node %s to node %s\n", str1, str2);
 
 
-void read_graph_from_file(char *filename, int *N, int**row_ptr, int **col_idx, double **val){
+        
+    }
 
+    if (line)
+        free(line);
 
-
-
+    fclose(fp);
 }
