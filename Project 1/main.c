e226777a130e3188a0aa11476394b6c3612e895e
Sakarias Frette <sakarias.frette@outlook.com>
2022-03-11 12:03:14 +0100

    added code infrastructure

---
+++ Project 1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <string.h>
+
+
+int main(int argc, char **argv){
+    char word[256];
+    
+    double d = atof(argv[2]);
+    double epsilon = atof(argv[3]);
+    int n = atoi(argv[4]);
+
+}
---
+++ Project 1/read_graph_from_file.c
@@ -0,0 +1,8 @@
+
+
+void read_graph_from_file(char *filename, int *N, int**row_ptr, int **col_idx, double **val){
+
+
+
+
+}
