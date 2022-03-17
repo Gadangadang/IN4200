7b6d89bfe7a2b63b686518445dc80c5146919caa
Gadangadang <sakarias.frette@outlook.com>
2022-03-17 17:51:33 +0100

    Found faster sorting

--- Project 1/top_n_webpages.c
+++ Project 1/top_n_webpages.c
@@ -20,7 +20,7 @@
     double tmp[N];
     int tmp2[N];
     //sort(scores, score_web_page_num, N);
-    mergesortV2(scores, N, tmp, score_web_page_num, tmp2);
+    superfastsort(scores, N, tmp, score_web_page_num, tmp2);
 
     /* Print top n webpages */
 
@@ -42,6 +42,55 @@
 
 
 
+int compare_exchange(double *a, double *b, int *c, int *d){
+    double tmp;
+    int tmp2;
+    if (*a > *b) {
+        tmp = *a;
+        *a = *b;
+        *b = tmp;
+
+        tmp2 = *c;
+        *c = *d;
+        *d = tmp2;
+        return 1;
+    }
+    return 0;
+}
+
+int slowsort(double *a, int *b, int N){
+    int r =N%2 - 1; // Used in the even step.
+    int change_even = 0;
+    int change_odd = 0;
+    #pragma omp parallel if (N > 1500)
+    {
+        for (size_t i = 0; i <N; i++) {
+            #pragma omp for reduction(+:change_odd)
+            for (size_t j = 0; j <N/2; j++) {
+                if(compare_exchange(&a[2*j], &a[2*j+1], &b[2*j], &b[2*j+1])){
+                    change_odd = 1;
+                }
+            }
+            #pragma omp for reduction(+:change_even)
+            for (size_t j = 0; j <N/2 + r; j++) {
+                if(compare_exchange(&a[2*j+1], &a[2*j+2], &b[2*j+1], &b[2*j+2])){
+                    change_even = 1;
+                }
+            }
+
+            if (!change_even && !change_odd){
+                break;
+            }
+            #pragma omp barrier
+            #pragma omp single
+            {
+                change_even = change_odd = 0;
+            }
+            
+        }
+    }
+    return 0;
+}
 
 void merge(double *X, int n, double *tmp, int *Y, int *tmp2)
 {
@@ -86,16 +135,16 @@ void merge(double *X, int n, double *tmp, int *Y, int *tmp2)
 
 } // end of merge()
 
-void mergesortV2(double *X, int n, double *tmp, int *Y, int *tmp2)
+void superfastsort(double *X, int n, double *tmp, int *Y, int *tmp2)
 {
     if (n < 2)
         return;
 
 #pragma omp task firstprivate(X, n, tmp, Y, tmp2)
-    mergesortV2(X, n / 2, tmp, Y, tmp2);
+    superfastsort(X, n / 2, tmp, Y, tmp2);
 
 #pragma omp task firstprivate(X, n, tmp, Y, tmp2)
-    mergesortV2(X + (n / 2), n - (n / 2), tmp, Y + (n / 2), tmp2);
+    superfastsort(X + (n / 2), n - (n / 2), tmp, Y + (n / 2), tmp2);
 
 //Wait for both paralel tasks to complete execution
 #pragma omp taskwait
