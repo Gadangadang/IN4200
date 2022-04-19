b84c6783a544470b1e04ec1c8c022b736ee9a3f8
Sakarias Frette <sakarias.frette@outlook.com>
2022-04-19 15:47:08 +0200

    readability from terminal

--- Project 2/IN4200-XXXXXX/serial_code/serial_main.c
+++ Project 2/IN4200-XXXXXX/serial_code/serial_main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../simple-jpeg/import_export_jpeg.h"
 #include "../utilities/functions.h"
-#include "../utilities/image.h"
+
+
 
 int main(int argc, char **argv){
     int m, n, c, iters;
@@ -11,8 +14,17 @@ int main(int argc, char **argv){
     char *input_jpeg_filename, *output_jpeg_filename;
     /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
     /* ... */
+    
+    kappa = atof(argv[1]); iters = atoi(argv[2]); 
+    input_jpeg_filename = argv[3];
+    output_jpeg_filename = argv[4];
+
+    
+    
+
     import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
     allocate_image (&u, m, n);
+
     allocate_image (&u_bar, m, n);
     convert_jpeg_to_image (image_chars, &u);
     iso_diffusion_denoising (&u, &u_bar, kappa, iters);
@@ -20,12 +32,8 @@ int main(int argc, char **argv){
     export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
     deallocate_image (&u);
     deallocate_image (&u_bar);
+
 return 0;
 }
 
 
-void allocate_image(image *u, int m, int n);
-void deallocate_image(image *u);
-void convert_jpeg_to_image(const unsigned char* image_chars, image *u);
-void convert_image_to_jpeg(const image *u, unsigned char* image_chars);
-void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters);
--- Project 2/IN4200-XXXXXX/utilities/functions.c
+++ Project 2/IN4200-XXXXXX/utilities/functions.c
@@ -1,14 +1,30 @@
 #include "functions.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 
 
 void deallocate_image(image *u){
-    
+    free(*u->image_data);
 }
 
 void allocate_image(image *u, int m, int n){
-    
+    u->image_data = malloc(m*sizeof(float));
+    for (int i= 0; i < m; i++){
+        u->image_data[i] = malloc(n*sizeof(float));
+        for (int j = 0; j < n; j++){
+            *u->image_data[i,j] = (float) 2.;
+        }
+    }
+
+    for (int i= 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            printf("%f",*u->image_data[i,j]);
+        }
+        printf("\n");
+    }
 }
+
 
 void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
     
