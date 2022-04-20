28a5daa62a31a26317b981a26510a64fd4ff0047
Sakarias Frette <sakarias.frette@outlook.com>
2022-04-20 11:22:08 +0200

    added serial functions, works

--- Project 2/IN4200-XXXXXX/serial_code/serial_main.c
+++ Project 2/IN4200-XXXXXX/serial_code/serial_main.c
@@ -7,24 +7,21 @@
 
 
 int main(int argc, char **argv){
+    /* Remember to change gcc to gcc-11 of running on mac m1 */
     int m, n, c, iters;
     float kappa;
     image u, u_bar;
     unsigned char *image_chars;
     char *input_jpeg_filename, *output_jpeg_filename;
     /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
-    /* ... */
-    
+
     kappa = atof(argv[1]); iters = atoi(argv[2]); 
     input_jpeg_filename = argv[3];
     output_jpeg_filename = argv[4];
 
-    
-    
 
     import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
     allocate_image (&u, m, n);
-
     allocate_image (&u_bar, m, n);
     convert_jpeg_to_image (image_chars, &u);
     iso_diffusion_denoising (&u, &u_bar, kappa, iters);
@@ -32,6 +29,8 @@ int main(int argc, char **argv){
     export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
     deallocate_image (&u);
     deallocate_image (&u_bar);
+
+    free(image_chars);
 
 return 0;
 }
