e9e96afa9d4a563984d19676137b0350b1a54478
Sakarias Frette <sakarias.frette@outlook.com>
2022-04-20 11:30:39 +0200

    added print for continued control

--- Project 2/IN4200-XXXXXX/serial_code/serial_main.c
+++ Project 2/IN4200-XXXXXX/serial_code/serial_main.c
@@ -22,9 +22,11 @@ int main(int argc, char **argv){
 
     import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
     allocate_image (&u, m, n);
+    printf("Image allocated, pixel dimension: [%d, %d]\n", m, n);
     allocate_image (&u_bar, m, n);
     convert_jpeg_to_image (image_chars, &u);
     iso_diffusion_denoising (&u, &u_bar, kappa, iters);
+    printf("Image denoising done\n");
     convert_image_to_jpeg (&u_bar, image_chars);
     export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
     deallocate_image (&u);
